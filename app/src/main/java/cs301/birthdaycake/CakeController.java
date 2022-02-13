d22becdd5d867c2d9c014e799a1fcccdf5af9ab0
Keoni <han24@up.edu>
2022-02-13 12:48:42 -0800

    Lab 4

---
+++ app/src/main/java/cs301/birthdaycake/CakeController.java
@@ -0,0 +1,52 @@
+package cs301.birthdaycake;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+
+public class CakeController implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, SeekBar.OnSeekBarChangeListener {
+    private CakeView aCakeView;
+    private CakeModel aCakeModel;
+
+
+    public CakeController(CakeView cvReference) {
+        aCakeView = cvReference;
+        aCakeModel = aCakeView.getCakeModelReference();
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d("tag", "Clicked");
+    }
+
+    public CakeModel getCakeModel() {
+        return aCakeModel;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if(b == true) {
+            aCakeModel.candlePresence = true;
+        }
+        else {
+            aCakeModel.candlePresence = false;
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        aCakeModel.candleCount = i;
+        aCakeView.invalidate();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
---
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -0,0 +1,85 @@
+package cs301.birthdaycake;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    //instance variable pointing to CakeView object in GUI
+    public CakeView reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_main);
+
+        // Insert findviewByID() Here
+
+        final CakeView CakeViewreference = findViewById(R.id.cakeviewID);
+
+        final CakeController CakeControlObject = new CakeController(CakeViewreference);
+
+        Button blowOutButton = findViewById(R.id.BlowOutButton);
+        blowOutButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Log.d("tag", "Blow Out Button Clicked");
+                        CakeControlObject.getCakeModel().candleLitStatus = false;
+                        CakeViewreference.invalidate();
+                    }
+                });
+
+        CompoundButton candleToggleSwitch = findViewById(R.id.CandleToggle);
+        candleToggleSwitch.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                        if (b == true) {
+                            CakeControlObject.getCakeModel().candlePresence = true;
+                        } else if (b != true) {
+                            CakeControlObject.getCakeModel().candlePresence = false;
+                        }
+                        CakeViewreference.invalidate();
+                    }
+                }
+        );
+
+        SeekBar candleCounterSeekBar = findViewById(R.id.candleCounter);
+        candleCounterSeekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                        CakeControlObject.getCakeModel().candleCount = i;
+                        //System.out.println(CakeControlObject.getCakeModel().candleCount);
+                        CakeViewreference.invalidate();
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+
+    }
+
+    public void goodBye(View button) {
+        Log.i("button", "Goodbye");
+    }
+
+}
